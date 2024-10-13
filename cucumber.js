0bb5f3c02b95e804d9b8aa925ac71e4c754fc193
John Ferguson Smart <john.smart@wakaleo.com>
2024-10-13 17:29:12 +0100

    Failing Cucumber tests

--- cucumber.js
+++ cucumber.js
@@ -1,7 +1,7 @@
 export default {
   import: ['features/**/*.js'],
   default: {
-    require: ['features/step_definitions/*.js'], // Load ES module step definitions
+    require: ['features/**/*.steps.js'], // Load ES module step definitions
     format: ['json:reports/report.json', 'html:reports/report.html'],
     paths: ['features/**/*.feature']
   }
