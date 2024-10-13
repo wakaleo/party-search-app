961caa4f7c9ff37cd877de9b9c72f47e141195c8
John Ferguson Smart <john.smart@wakaleo.com>
2024-10-13 16:47:38 +0100

    Configuration tweaks

--- cucumber.js
+++ cucumber.js
@@ -1,7 +1,6 @@
 export default {
   import: ['features/**/*.js'],
   default: {
-    requireModule: ['@playwright/test'], // Ensures Playwright is loaded
     require: ['features/step_definitions/*.js'], // Load ES module step definitions
     format: ['json:reports/report.json', 'html:reports/report.html'],
     paths: ['features/**/*.feature']
